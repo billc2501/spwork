746e47cc1900d700b451accc443872ced7c8081f
Billy Choi <billychoi@Billys-MacBook-Air.local>
2022-09-30 12:22:29 -0700

    file

--- spworkanother/App.js
+++ spworkanother/App.js
@@ -7,7 +7,7 @@ import {decode, encode} from 'base-64'
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
 
-const Stack = createStackNavigtor();
+const Stack = createStackNavigator();
 
 export default function App() {
 
